cc5444cf89f3e450e04f2af8ce631b1db0a34d5d
YugoAnzai <yugoanzai@gmail.com>
2020-10-31 19:00:41 -0300

    Puller dynamic but light. UI update. Set target framereart

---
+++ Assets/Scripts/HelperComponents/SetTargetFramerate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTargetFramerate : MonoBehaviour
+{
+    
+    [SerializeField] private int targetFramerate = 60;
+
+    void Awake()
+    {
+        Application.targetFrameRate = targetFramerate;
+        YugoA.Helpers.LogHelper.Log($"Framerate Set: {targetFramerate}", Color.yellow);
+    }
+
+}
